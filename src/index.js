f1cb88f09c4849583eb1022d9d5d86c90efbfccd
Bilal <bks1733@gmail.com>
2023-06-07 16:50:55 +0530

    react version issue resolved.

--- src/index.js
+++ src/index.js
@@ -1,24 +1,25 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { combineReducers, createStore } from 'redux';
-import { BrowserRouter } from 'react-router-dom';
-
+import ReactDOM from 'react-dom/client';
+import {BrowserRouter} from 'react-router-dom';
+ 
+import {legacy_createStore as createStore, combineReducers} from 'redux';
+ 
 import './index.css';
 import App from './App';
 import productReducer from './store/reducers/products';
-
+import { Provider } from 'react-redux';
+ 
 const rootReducer = combineReducers({
-  shop: productReducer,
+    shop: productReducer,
 });
-
+ 
 const store = createStore(rootReducer);
-
+ 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </Provider>
+    <Provider store={store}>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </Provider>
 );
